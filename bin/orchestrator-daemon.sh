455eca10ecb53a5e0c2f70dfb97a6f045c244f43
Alvaro Vega <avega@tid.es>
2015-02-11 12:15:48 +0100

    fix a tipo in orchestrator-daemon script

--- bin/orchestrator-daemon.sh
+++ bin/orchestrator-daemon.sh
@@ -11,11 +11,11 @@
 . /etc/rc.d/init.d/functions
 
 CURR="$( cd "$( dirname "$( readlink -f ${BASH_SOURCE[0]} )" )" && pwd )"
-ORCHESTRATOR_DIR=usr/lib/python2.6/site-packages/iotp-orchestrator
+ORCHESTRATOR_DIR=/usr/lib/python2.6/site-packages/iotp-orchestrator
 pname="orchestrator"
 user="orchestrator"
 
-exe="/usr/bin/python ./manage.py runserver 0.0.0.0:8084 --setings=settings.dev"
+exe="/usr/bin/python ./manage.py runserver 0.0.0.0:8084 --settings=settings.dev"
 
 server="$exe"
 
