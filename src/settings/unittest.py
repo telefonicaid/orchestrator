d2ed45b6651ce1de7f4af8d927e67f2a5d0d83bd
Alvaro Vega <avega@tid.es>
2015-10-26 08:29:00 +0100

    fix unit tests

--- src/settings/unittest.py
+++ src/settings/unittest.py
@@ -41,6 +41,7 @@ INSTALLED_APPS += (
 )
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+PROJECT_ROOT= '.'
 COBERTURA_DIR = os.path.join(PROJECT_ROOT, 'target', 'site', 'cobertura')
 UNIT_TESTS_DIR = os.path.join(PROJECT_ROOT, 'target', 'surefire-reports')
 if not os.path.exists(COBERTURA_DIR):
@@ -65,13 +66,13 @@ NOSE_ARGS = [
 
 KEYSTONE = {
     "host": "localhost",
-    "port": "5001",
+    "port": "5000",
     "protocol":"http"
 }
 
 KEYPASS = {
     "host": "localhost",
-    "port": "7070",
+    "port": "8080",
     "protocol":"http"
 }
 
