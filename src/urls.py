6b22e72f0b6b94bebec4ab809e718be97bb8557b
Alvaro Vega <avega@tid.es>
2016-01-29 11:42:43 +0100

    remove api-auth

--- src/urls.py
+++ src/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import url, include, patterns
 
 urlpatterns = patterns('',
     url(r'^v1.0/', include('orchestrator.api.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
