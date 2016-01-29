8a60e31c933efad2eb0dcdab57b8d3f7003d91d1
Alvaro Vega <avega@tid.es>
2016-01-29 08:41:03 +0100

    remove redundant check

--- src/orchestrator/core/flow/base.py
+++ src/orchestrator/core/flow/base.py
@@ -125,14 +125,13 @@ class FlowBase(object):
         if iot_module in self.endpoints:
             return self.endpoints[iot_module]
         else:
-            if iot_module in IOTMODULES:
-                comppackage = __import__("settings.dev", fromlist=iot_module)
-                iot_module_conf = getattr(comppackage, iot_module)
-                assert 'protocol' in iot_module_conf
-                assert 'host' in iot_module_conf
-                assert 'port' in iot_module_conf
-                iot_mddule_enpoint = iot_module_conf['protocol'] + "://" + \
-                  iot_module_conf['host'] + ":" + \
-                  iot_module_conf['port'] + "/notify"
-                self.endpoints[iot_module] = iot_mddule_enpoint
-                return iot_mddule_enpoint
+            comppackage = __import__("settings.dev", fromlist=iot_module)
+            iot_module_conf = getattr(comppackage, iot_module)
+            assert 'protocol' in iot_module_conf
+            assert 'host' in iot_module_conf
+            assert 'port' in iot_module_conf
+            iot_mddule_enpoint = iot_module_conf['protocol'] + "://" + \
+              iot_module_conf['host'] + ":" + \
+              iot_module_conf['port'] + "/notify"
+            self.endpoints[iot_module] = iot_mddule_enpoint
+            return iot_mddule_enpoint
