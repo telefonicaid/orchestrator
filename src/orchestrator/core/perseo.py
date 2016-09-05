d32fcad7282737414158a42a01aae8cff1efe2ab
Alvaro Vega <avega@tid.es>
2016-09-05 08:46:41 +0200

    set RestOperations endpoint name

--- src/orchestrator/core/perseo.py
+++ src/orchestrator/core/perseo.py
@@ -44,7 +44,8 @@ class PerseoOperations(object):
         self.PERSEO_HOST = PERSEO_HOST
         self.PERSEO_PORT = PERSEO_PORT
 
-        self.PerseoRestOperations = RestOperations(PERSEO_PROTOCOL,
+        self.PerseoRestOperations = RestOperations("PERSEO",
+                                                   PERSEO_PROTOCOL,
                                                    PERSEO_HOST,
                                                    PERSEO_PORT,
                                                    CORRELATOR_ID,
