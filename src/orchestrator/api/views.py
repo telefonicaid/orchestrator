51d467197a633bb2d158157cafdfc0570348e8f9
Alvaro Vega <avega@tid.es>
2016-10-26 15:55:49 +0200

    add missed filters to django loggers

--- src/orchestrator/api/views.py
+++ src/orchestrator/api/views.py
@@ -48,7 +48,16 @@ from orchestrator.core.flow.Users import Users
 from orchestrator.api import parsers
 
 
+from orchestrator.common.util import ContextFilterCorrelatorId
+from orchestrator.common.util import ContextFilterTransactionId
+from orchestrator.common.util import ContextFilterService
+from orchestrator.common.util import ContextFilterSubService
+
 logger = logging.getLogger('orchestrator_api')
+logger.addFilter(ContextFilterCorrelatorId("n/a"))
+logger.addFilter(ContextFilterTransactionId("n/a"))
+logger.addFilter(ContextFilterService("None"))
+logger.addFilter(ContextFilterSubService(""))
 
 
 class Stats(object):
