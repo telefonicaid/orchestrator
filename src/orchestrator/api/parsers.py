adcf22c1095acc8137ed75bca714811aa16d908d
Alvaro Vega <avega@tid.es>
2016-10-26 15:55:49 +0200

    add missed filters to django loggers

--- src/orchestrator/api/parsers.py
+++ src/orchestrator/api/parsers.py
@@ -5,7 +5,17 @@ from rest_framework.parsers import JSONParser
 
 from orchestrator.api import schemas
 
+from orchestrator.common.util import ContextFilterCorrelatorId
+from orchestrator.common.util import ContextFilterTransactionId
+from orchestrator.common.util import ContextFilterService
+from orchestrator.common.util import ContextFilterSubService
+
 logger = logging.getLogger('orchestrator_api')
+logger.addFilter(ContextFilterCorrelatorId("n/a"))
+logger.addFilter(ContextFilterTransactionId("n/a"))
+logger.addFilter(ContextFilterService("None"))
+logger.addFilter(ContextFilterSubService(""))
+
 class JSONSchemaParser(JSONParser):
  
     def parse(self, stream, media_type=None, parser_context=None):
