344ca3936c9b5e25357a8b08de6dcd458e9c47c7
Rafael Gonzalez <rafael@gonzalezfuentetaja@telefonica.com>
2016-11-08 12:36:52 +0100

    added --with-version and --with-release options

--- package-orchestrator.sh
+++ package-orchestrator.sh
@@ -9,8 +9,22 @@ source $BASE/get_version_string.sh
 
 #read ver rel < <(get_rpm_version_string)
 string=$(get_rpm_version_string)
-ver=${string% *}
-rel=${string#* }
+VERSION_VALUE=${string% *}
+RELEASE_VALUE=${string#* }
+
+args=("$@")
+ELEMENTS=${#args[@]}
+
+for (( i=0;i<$ELEMENTS;i++)); do
+    arg=${args[${i}]}
+    if [ "$arg" == "--with-version" ]; then
+        VERSION_VALUE=${args[${i}+1]}
+    fi
+    if [ "$arg" == "--with-release" ]; then
+        RELEASE_VALUE=${args[${i}+1]}
+    fi
+done
+
 
 RPM_DIR=$BASE/build/rpm
 mkdir -p $RPM_DIR/BUILD
@@ -21,5 +35,5 @@ rpmbuild -bb orchestrator.spec \
   --define "_topdir $RPM_DIR" \
   --define "_root $BASE"\
   --define "_project_user $ORCHESTRATOR_USER"\
-  --define "_version $ver"\
-  --define "_release $rel"
+  --define "_version $VERSION_VALUE"\
+  --define "_release $RELEASE_VALUE"
