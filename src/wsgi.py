f8b47c7e5ecd65369d7db27760c4f0505694bbb7
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2019-02-14 14:28:08 +0100

    fix startup orchestrator

--- src/wsgi.py
+++ src/wsgi.py
@@ -18,16 +18,15 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orchestrator.settings.dev")
 
 
-import orchestrator.core.startup as startup
-startup.run()
-
-
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+
+#
+# Orchestrator startup
+#
+import orchestrator.core.startup as startup
+startup.run()
