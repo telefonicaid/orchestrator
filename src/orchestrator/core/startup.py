e18a776cd34d477218b2762049aa675679b36678
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2019-04-15 09:06:14 +0200

    Update startup.py

--- src/orchestrator/core/startup.py
+++ src/orchestrator/core/startup.py
@@ -143,8 +143,7 @@ def check_endpoints():
     # Mailer: optional
     MAILER_HOST = settings.MAILER['host']
     MAILER_PORT = settings.MAILER['port']
-    MAILER_TLS = settings.MAILER['tls']
-    mailer = MailerOperations(MAILER_HOST, MAILER_PORT, MAILER_TLS)
+    mailer = MailerOperations(MAILER_HOST, MAILER_PORT)
     try:
         mailer.checkMailer()
         logger.info("MAILER endpoint OK")
