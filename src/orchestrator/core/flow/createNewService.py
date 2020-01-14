f9303b351224bcb1f18d704dfe3d2c7285152d46
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2020-01-14 11:01:05 +0100

    Update createNewService.py

--- src/orchestrator/core/flow/createNewService.py
+++ src/orchestrator/core/flow/createNewService.py
@@ -306,90 +306,90 @@ class CreateNewService(FlowBase):
                                         ID_NEW_SERVICE_ROLE_ADMIN_SET[component],
                                         POLICY_FILE_NAME='policy-'+component+'-serviceadmin.xml')
 
-            #
-            # 6. Create groups for new service (aka domain)
-            #
-            ADMIN_GROUP_NAME = "AdminGroup"
-            SERVICE_CUSTOMER_GROUP_NAME = "ServiceCustomerGroup"
-            SUB_SERVICE_ADMIN_GROUP_NAME = "SubServiceAdminGroup"
-            SUB_SERVICE_CUSTOMER_GROUP_NAME = "SubServiceCustomerGroup"
+#            #
+#            # 6. Create groups for new service (aka domain)
+#            #
+#            ADMIN_GROUP_NAME = "AdminGroup"
+#            SERVICE_CUSTOMER_GROUP_NAME = "ServiceCustomerGroup"
+#            SUB_SERVICE_ADMIN_GROUP_NAME = "SubServiceAdminGroup"
+#            SUB_SERVICE_CUSTOMER_GROUP_NAME = "SubServiceCustomerGroup"
 
-            try:
-                ID_ADMIN_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
-                                                      ID_DOM1,
-                                                      NEW_SERVICE_NAME,
-                                                      ADMIN_GROUP_NAME,
-                                                      None)
+#            try:
+#                ID_ADMIN_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
+#                                                      ID_DOM1,
+#                                                      NEW_SERVICE_NAME,
+#                                                      ADMIN_GROUP_NAME,
+#                                                      None)
 
-                ID_SERVICE_CUSTOMER_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
-                                                      ID_DOM1,
-                                                      NEW_SERVICE_NAME,
-                                                      SERVICE_CUSTOMER_GROUP_NAME,
-                                                      None)
+#                ID_SERVICE_CUSTOMER_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
+#                                                      ID_DOM1,
+#                                                      NEW_SERVICE_NAME,
+#                                                      SERVICE_CUSTOMER_GROUP_NAME,
+#                                                      None)
 
-                ID_SUB_SERVICE_ADMIN_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
-                                                      ID_DOM1,
-                                                      NEW_SERVICE_NAME,
-                                                      SUB_SERVICE_ADMIN_GROUP_NAME,
-                                                      None)
+#                ID_SUB_SERVICE_ADMIN_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
+#                                                      ID_DOM1,
+#                                                      NEW_SERVICE_NAME,
+#                                                      SUB_SERVICE_ADMIN_GROUP_NAME,
+#                                                      None)
 
-                ID_SUB_SERVICE_CUSTOMER_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
-                                                      ID_DOM1,
-                                                      NEW_SERVICE_NAME,
-                                                      SUB_SERVICE_CUSTOMER_GROUP_NAME,
-                                                      None)
-
-
-            except Exception, ex:
-                self.logger.warn("ERROR creating groups  %s" % (
-                    ex))
-                self.logger.info("removing uncomplete created domain %s" % ID_DOM1)
-                try:
-                    self.idm.disableDomain(DOMAIN_ADMIN_TOKEN, ID_DOM1)
-                    self.idm.deleteDomain(DOMAIN_ADMIN_TOKEN, ID_DOM1)
-                except Exception, ex:
-                    self.logger.warn("%s trying to remove uncomplete created domain %s" % (ex, ID_DOM1))
-                return self.composeErrorCode(ex)
-
-            self.logger.debug("ID of group %s: %s" % (ADMIN_GROUP_NAME,
-                                                      ID_ADMIN_GROUP))
-            self.logger.debug("ID of group %s: %s" % (SERVICE_CUSTOMER_GROUP_NAME,
-                                                      ID_SERVICE_CUSTOMER_GROUP))
-            self.logger.debug("ID of group %s: %s" % (SUB_SERVICE_ADMIN_GROUP_NAME,
-                                                      ID_SUB_SERVICE_ADMIN_GROUP))
-            self.logger.debug("ID of group %s: %s" % (SUB_SERVICE_CUSTOMER_GROUP_NAME,
-                                                      ID_SUB_SERVICE_CUSTOMER_GROUP))
+#                ID_SUB_SERVICE_CUSTOMER_GROUP = self.idm.createGroupDomain(DOMAIN_ADMIN_TOKEN,
+#                                                      ID_DOM1,
+#                                                      NEW_SERVICE_NAME,
+#                                                      SUB_SERVICE_CUSTOMER_GROUP_NAME,
+#                                                      None)
 
 
-            self.idm.grantDomainRoleToGroup(DOMAIN_ADMIN_TOKEN,
-                                     ID_DOM1,
-                                     ID_ADMIN_GROUP,
-                                     ADMIN_ROLE_ID)
+#            except Exception, ex:
+#                self.logger.warn("ERROR creating groups  %s" % (
+#                    ex))
+#                self.logger.info("removing uncomplete created domain %s" % ID_DOM1)
+#                try:
+#                    self.idm.disableDomain(DOMAIN_ADMIN_TOKEN, ID_DOM1)
+#                    self.idm.deleteDomain(DOMAIN_ADMIN_TOKEN, ID_DOM1)
+#                except Exception, ex:
+#                    self.logger.warn("%s trying to remove uncomplete created domain %s" % (ex, ID_DOM1))
+#                return self.composeErrorCode(ex)
 
-            self.idm.grantDomainRoleToGroup(DOMAIN_ADMIN_TOKEN,
-                                     ID_DOM1,
-                                     ID_SERVICE_CUSTOMER_GROUP,
-                                     ID_NEW_SERVICE_ROLE_SERVICECUSTOMER)
+#            self.logger.debug("ID of group %s: %s" % (ADMIN_GROUP_NAME,
+#                                                      ID_ADMIN_GROUP))
+#            self.logger.debug("ID of group %s: %s" % (SERVICE_CUSTOMER_GROUP_NAME,
+#                                                      ID_SERVICE_CUSTOMER_GROUP))
+#            self.logger.debug("ID of group %s: %s" % (SUB_SERVICE_ADMIN_GROUP_NAME,
+#                                                      ID_SUB_SERVICE_ADMIN_GROUP))
+#            self.logger.debug("ID of group %s: %s" % (SUB_SERVICE_CUSTOMER_GROUP_NAME,
+#                                                      ID_SUB_SERVICE_CUSTOMER_GROUP))
 
-            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
-                                      ID_DOM1,
-                                      ID_ADMIN_GROUP,
-                                      ID_NEW_SERVICE_ROLE_SUBSERVICEADMIN)
 
-            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
-                                      ID_DOM1,
-                                      ID_SERVICE_CUSTOMER_GROUP,
-                                      ID_NEW_SERVICE_ROLE_SUBSERVICECUSTOMER)
+#            self.idm.grantDomainRoleToGroup(DOMAIN_ADMIN_TOKEN,
+#                                     ID_DOM1,
+#                                     ID_ADMIN_GROUP,
+#                                     ADMIN_ROLE_ID)
 
-            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
-                                      ID_DOM1,
-                                      ID_SUB_SERVICE_ADMIN_GROUP,
-                                      ID_NEW_SERVICE_ROLE_SUBSERVICEADMIN)
+#            self.idm.grantDomainRoleToGroup(DOMAIN_ADMIN_TOKEN,
+#                                     ID_DOM1,
+#                                     ID_SERVICE_CUSTOMER_GROUP,
+#                                     ID_NEW_SERVICE_ROLE_SERVICECUSTOMER)
 
-            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
-                                      ID_DOM1,
-                                      ID_SUB_SERVICE_CUSTOMER_GROUP,
-                                      ID_NEW_SERVICE_ROLE_SUBSERVICECUSTOMER)
+#            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
+#                                      ID_DOM1,
+#                                      ID_ADMIN_GROUP,
+#                                      ID_NEW_SERVICE_ROLE_SUBSERVICEADMIN)
+
+#            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
+#                                      ID_DOM1,
+#                                      ID_SERVICE_CUSTOMER_GROUP,
+#                                      ID_NEW_SERVICE_ROLE_SUBSERVICECUSTOMER)
+
+#            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
+#                                      ID_DOM1,
+#                                      ID_SUB_SERVICE_ADMIN_GROUP,
+#                                      ID_NEW_SERVICE_ROLE_SUBSERVICEADMIN)
+
+#            self.idm.grantInheritRoleToGroup(NEW_SERVICE_ADMIN_TOKEN,
+#                                      ID_DOM1,
+#                                      ID_SUB_SERVICE_CUSTOMER_GROUP,
+#                                      ID_NEW_SERVICE_ROLE_SUBSERVICECUSTOMER)
 
             #
             # 7. Create MongoDB indexes
